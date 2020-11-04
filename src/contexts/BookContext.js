0e8ef8f152387174e5c9856a5a6a4351ac471093
Aarush Goyal <64161383+Aarush-Goyal@users.noreply.github.com>
2020-11-04 22:10:46 +0530

    emptied array booklist

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -3,22 +3,7 @@ import { v4 as uuid } from "uuid";
 export const BookContext = createContext();
 
 function BookContextProvider(props) {
-  const [books, setBooks] = useState([
-    { id: uuid(), title: "sapiens", author: "Charlie Chaplin" },
-    { id: uuid(), title: "Shoe Dog Phil Knight", author: "Daya and Abhijeet" },
-    { id: uuid(), title: "Rich Dad Poor Dad", author: "Robert Kiyosaki" },
-    {
-      id: uuid(),
-      title: "Good Economics for bad times",
-      author: "Abhijit Banerjee",
-    },
-    {
-      id: uuid(),
-      title: "The Intelligent Investor",
-      author: "Benjamin Graham",
-    },
-    { id: uuid(), title: "The EveryThing Store", author: "Brad Stone" },
-  ]);
+  const [books, setBooks] = useState([]);
 
   const [booksRead, setBooksRead] = useState([]);
   const addBooks = (title, author) => {
